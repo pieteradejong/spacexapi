96a4543ac6a74a8565d79650c19bafe224607018
Pieter de Jong <pieter.a.dejong@gmail.com>
2023-07-28 20:10:03 -0400

    add root and status endpoints

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI
-from motor.motor_asyncio import AsyncIOMotorClient
-from models import *
+# from motor.motor_asyncio import AsyncIOMotorClient
+# from models import *
 
 app = FastAPI()
 
+"""
+Enable these once we want to connect to the MongoDB database
 @app.on_event('startup')
 async def startup_db_client():
     app.mongodb_client = AsyncIOMotorClient('mongodb://localhost:27017')
@@ -12,4 +14,13 @@ async def startup_db_client():
 @app.on_event('shutdown')
 async def shutdown_db_client():
     app.mongodb_client.close()
+"""
+
+@app.get('/')
+def read_root():
+    return {'message': 'Welcome to this FastAPI application for SpaceX launch data.'}
+
+@app.get('/status')
+def read_status():
+    return {'status': 'running'}
 
